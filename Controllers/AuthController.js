4ce1ce15100fc99a75e498851d40900d4a53fb3e
Moaane <gpro0150@gmail.com>
2023-07-27 11:15:02 +0700

    update

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,1 +1,31 @@
-    
+
+const signUp = async (req, res) => {
+    const { username, password, confirmPassword } = req.body
+    try {
+        if (password != confirmPassword) {
+            return res.status(400).json({ msg: "Password Not Match" })
+        }
+        const isUserValid = await usersLogin.findFirst({
+            where: {
+                username: username
+            }
+        })
+        if (!isUserValid) {
+            const createUsername = username
+            // const hashPassword = await bycrypt.hash(password, 10)
+
+            const data = await usersLogin.create({
+                datas: {
+                    username: createUsername,
+                    password: bcrypt.hashSync(req.body.password, 8),
+                }
+            }).then()
+
+            res.json({ msg: "Success Register" })
+        } else {
+            return res.status(400).json({ msg: "User Is Available, try another username" })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
