6b3ae93c0338e0a78d67444ec4fca9099a8bf789
FathirTHX <fathirisgaming12@gmail.com>
2023-07-27 11:22:30 +0700

    Update

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,36 +1,50 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
-const { PrismaClient } = require('@prisma/client')  
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
-const user = prisma.user 
+const user = prisma.user
 
-const SignIn = async (req, res) =>{
+const SignIn = async (req, res) => {
+    //mengambil data dari metode post
     const { username, password } = req.body
     try {
+        //mencari data user yang mencoba login
         const isUserValid = await user.findFirst({
-            where:{
+            where: {
                 username: username,
                 password: password,
             }
         })
 
+        //mengecek apakah user nya ketemu atau tidak, jika tidak ketemu maka fungsi login ini akan berakhir
         if (!isUserValid) {
-            return res.status(400).json({msg:"user not found"})
+            return res.status(400).json({ msg: "user not found" })
         }
 
-        const isPasswordValid = await bcrypt.compare(password, isPasswordValid.password)
-            if (!isPasswordValid) {
-                return res.status(400).json({msg:"password is not valid"})
-            }
-        
-            const secretKey = crypto.randomBytes(10).toString('hex')
-            const token = jwt.sign({id: isUserValid.id}, secretKey)
-            const datas = {
-                'token' : token,
-            }
+        //mengecek apakah password yang di ketikan user ketika post data itu sama dengan password yang didapat dari mengecek isUserValid
+        const isPasswordValid = await bcrypt.compare(password, isUserValid.password)
+        if (!isPasswordValid) {
+            return res.status(400).json({ msg: "password is not valid" })
+        }
 
-            res.json(datas)
+        //menggenerate token secara random
+        const secretKey = crypto.randomBytes(10).toString('hex')
+        const token = jwt.sign({ id: isUserValid.id }, secretKey)
+
+        //mengecek user yang login itu siapa
+        const getRole = await user.findFirst({
+            where: {
+                username: username
+            }
+        })
+
+        const datas = {
+            'token': token,
+            'role': getRole.role,
+        }
+
+        res.json(datas)
 
     } catch (error) {
         console.log(error)
