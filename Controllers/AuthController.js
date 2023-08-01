afb801d04decbb2937b1ccbafcb9611ab95fa751
Moaane <gpro0150@gmail.com>
2023-08-01 13:59:58 +0700

    .

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -8,32 +8,27 @@ const cand = prisma.candidate
 const comp = prisma.company
 
 const SignIn = async (req, res) => {
-    //mengambil data dari metode post
     const { username, password } = req.body
     try {
-        //mencari data user yang mencoba login
         const isUserValid = await user.findFirst({
             where: {
                 username: username
             }
         })
 
-        //mengecek apakah user nya ketemu atau tidak, jika tidak ketemu maka fungsi login ini akan berakhir
         if (!isUserValid) {
             return res.status(400).json({ msg: "user not found" })
         }
 
-        //mengecek apakah password yang di ketikan user ketika post data itu sama dengan password yang didapat dari mengecek isUserValid
         const isPasswordValid = await bcrypt.compare(password, isUserValid.password)
+
         if (!isPasswordValid) {
             return res.status(400).json({ msg: "password is not valid" })
         }
 
-        //menggenerate token secara random
         const secretKey = crypto.randomBytes(10).toString('hex')
         const token = jwt.sign({ id: isUserValid.id }, secretKey)
 
-        //mengecek user yang login itu siapa
         const getRole = await user.findFirst({
             where: {
                 username: username
@@ -45,7 +40,9 @@ const SignIn = async (req, res) => {
                 id: getRole.companyId
             }
         })
-console.log(getCompany)
+
+        console.log(getCompany)
+        
         const datas = {
             'token': token,
             'role': getRole.role,
@@ -105,9 +102,11 @@ const SignInCand = async (req, res) => {
         }
 
         const isPasswordValid = await bcrypt.compare(password, isCandValid.password)
+
         if (!isPasswordValid) {
             return res.status(400).json({ msg: "password is not valid" })
         }
+
         const secretKey = crypto.randomBytes(10).toString('hex')
         const token = jwt.sign({ id: isCandValid.id }, secretKey)
 
